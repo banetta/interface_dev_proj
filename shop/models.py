853211478ef2245b64dbd5c81d1e5fe82a7504a2
banetta <banetion@gmail.com>
2021-08-02 09:10:00 +0900

    프로젝트 생성

---
+++ shop/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+# class Product(models.Model):
+#     p_id = models.PositiveBigIntegerField(db_column='p_Id', primary_key=True)  # Field name made lowercase.
+#     p_title = models.TextField()
+#     p_image = models.TextField()
+#     p_lprice = models.PositiveIntegerField()
+#     p_brand = models.CharField(max_length=45, blank=True, null=True)
+#     p_category1 = models.CharField(max_length=45, blank=True, null=True)
+#     p_category2 = models.CharField(max_length=45, blank=True, null=True)
+#     p_category3 = models.CharField(max_length=45, blank=True, null=True)
+#     p_category4 = models.CharField(max_length=45, blank=True, null=True)
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'product'
+
