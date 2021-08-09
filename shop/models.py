c79f226f76be37f6ce9fa95930083f1e311efba0
banetta <banetion@gmail.com>
2021-08-09 09:09:53 +0900

    Model변경 사항 적용

--- shop/models.py
+++ shop/models.py
@@ -24,6 +24,7 @@ class Review(models.Model):
     r_text = models.TextField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     r_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
 
     def __str__(self):
         return self.r_title
@@ -33,6 +34,7 @@ class ShopBasket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     s_ordered = models.BooleanField(default=False)
+    s_amount = models.IntegerField(default=1)
 
     def __str__(self):
         return "[{}] {}".format(self.id, self.user.username)
@@ -65,3 +67,12 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.a_title
+
+
+class Notice(models.Model):
+    n_title = models.CharField(max_length=200)
+    n_text = models.TextField()
+    n_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.n_title
